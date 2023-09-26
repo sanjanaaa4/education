4df825cf0b889d0e751ab1a68d3d517f3e76895a
Parnitha-1 <rajuriparnitha@gmail.com>
2023-09-26 18:27:14 +0530

    final commit

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-require('./app_server/models/db');
+var routesApi = require('./app_api/routes/index');
+require('./app_api/models/db');
 var app = express();
 
 // view engine setup
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api',routesApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,24 +1,19 @@
-require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 var ctrlLocations = require('../controller/locations');
-var ctrlOthers = require('../controller/others');
+var ctrlReviews = require('../controller/reviews');
+router.get('/locations', ctrlLocations.locationsListByDistance);
+router.post('/locations', ctrlLocations.locationsCreate);
+router.get('/locations/:locationid', ctrlLocations.locationsReadOne);
+router.put('/locations/:locationid', ctrlLocations.locationsUpdateOne);
+router.delete('/locations/:locationid', ctrlLocations.locationsDeleteOne);
+// reviews
+router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
+router.get('/locations/:locationid/reviews/:reviewid',
+ctrlReviews.reviewsReadOne);
 
-/* GET Locations page */
-router.get('/', ctrlLocations.homelist);
-
-router.get('/location', ctrlLocations.locationInfo);
-//router.get('/location1', ctrlLocations.locationInfo1);
-router.get('/location2', ctrlLocations.locationInfo2);
-router.get('/location/review/new', ctrlLocations.addReview);
-/*router.get('/location/review/new1', ctrlLocations.addReview1);
-router.get('/location/review/new2', ctrlLocations.addReview2);*/
-
-/*GET others page */
-router.get('/about', ctrlOthers.about);
-
-router.get('/signin', ctrlOthers.signin);
-
-router.get('/register', ctrlOthers.register);
-
+router.put('/locations/:locationid/reviews/:reviewid',
+ctrlReviews.reviewsUpdateOne);
+router.delete('/locations/:locationid/reviews/:reviewid',
+ctrlReviews.reviewsDeleteOne);
 module.exports = router;
